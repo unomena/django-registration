026632658b3d8ae23c8df651ee5d3e409d58594d
Euan Jonker <euan@unomena.com>
2012-01-10 15:55:30 +0200

    Bumped the version to 0.7.unomena.8

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ for dirpath, dirnames, filenames in os.walk('registration'):
 
 
 setup(name='django-registration',
-      version='0.7.unomena.7',
+      version='0.7.unomena.8',
       description='An extensible user-registration application for Django',
       author='James Bennett',
       author_email='james@b-list.org',
